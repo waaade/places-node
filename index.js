f55f7037267bff5895283e9f14ebed1f51db3df2
waaade <johnlarson562@gmail.com>
2019-12-11 17:50:19 -0700

    Search by name function in progress

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const request = require('request');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
@@ -7,7 +6,7 @@ const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
 
 
-var sql = 'SELECT * FROM types';
+var testQuery = 'SELECT * FROM types';
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -16,7 +15,7 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/testServer', function(req, res){
-    pool.query(sql, function(err, result) {
+    pool.query(testQuery, function(err, result) {
         if (err) {
             console.log("Error in query: ")
             console.log(err);
@@ -25,8 +24,8 @@ express()
         console.log(result.rows);
         res.json(result.rows);
     })})
-    .get('/search', (req, res) => res.render('pages/search'))
-    .get('/seeall', function(req, res){
+    .get('/search', (req, res) => res.render('pages/search')) //The search page
+    .get('/seeall', function(req, res){ //Get all the places
       pool.query('SELECT * FROM places', function(err, result) {
         if (err) {
           console.log("Error in query: ")
@@ -35,6 +34,17 @@ express()
         console.log("Back from DB with result:");
         console.log(result.rows);
         res.json(result.rows); 
+      })
+    })
+    .get('/searchbyname', function(req, res){
+      var name = req.query.name;
+      var sql = 'SELECT * FROM places WHERE name ILIKE ' + name;
+      pool.query(sql, function(err, result) {
+        if (err) {
+          console.log("Error in query: ")
+          console.log(err);
+        }
+        res.json(result.rows);
       })
     })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- public/client.js
+++ public/client.js
@@ -7,3 +7,13 @@ function displayAll() {
         
     })
 }
+
+function nameSearch() {
+    var name = $("#placename").val();
+    $.get("/searchbyname", {name:name}, function(data) {
+        for (var i = 0; i < data.length; i++) {
+            var item = data[i];
+            $("#nameresults").append("<li>" + item.name + "</li>");
+        }
+    })
+}
